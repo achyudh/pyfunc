ad3503ef64fd2288b4c7fdd835feb2afc887ebad
Achyudh K Ram <achyudhk@gmail.com>
2017-10-12 21:00:21 +0200

    Squash bugs in varExtractor

--- file_selector/varExtractor.py
+++ file_selector/varExtractor.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     retriever = Retriever()
     extractor = Extractor(None)
     # py_files = retriever.list_all_files_in_folder("../scikit-learn-master/sklearn")
-    py_files = retriever.list_all_files_in_folder("../test")
+    py_files = retriever.list_all_files_in_folder("../scikit-learn-master")
     output_json = {}
 
     for py_file in py_files:
@@ -19,26 +19,28 @@ if __name__ == "__main__":
         declarations = list()
 
         for node in func_def_nodes:
-            docstr_replaced = ast.get_docstring(node).replace("\n", "NEWLINE_PLACEHOLDER")
-            param_pairs, return_pairs = extractor.extract_variable_types_from_docstring(docstr_replaced)
+            if ast.get_docstring(node) is not None:
+                docstr_replaced = ast.get_docstring(node).replace("\n", "NEWLINE_PLACEHOLDER")
+                param_pairs, return_pairs = extractor.extract_variable_types_from_docstring(docstr_replaced)
 
-            # Hash the type information for faster searching
-            # TODO: Modify at source varExtractorRegEx, change list to dict
-            param_types = dict()
-            for pair in param_pairs:
-                param_types[pair[0]] = pair[1]
+                # Hash the type information for faster searching
+                # TODO: Modify at source varExtractorRegEx, change list to dict
+                param_types = dict()
+                if param_pairs is not None:
+                    for pair in param_pairs:
+                        param_types[pair[0]] = pair[1]
 
-            invalid_arg_ctr = 0
-            param_types_out = list()
-            for argument in node.args.args:
-                if argument.id in param_types:
-                    param_types_out.append((argument.id, param_types[argument.id]))
-                else:
-                    invalid_arg_ctr += 1
+                invalid_arg_ctr = 0
+                param_types_out = list()
+                for argument in node.args.args:
+                    if argument.id in param_types:
+                        param_types_out.append((argument.id, param_types[argument.id]))
+                    else:
+                        invalid_arg_ctr += 1
 
-            declarations.append((node.name, ((param_types_out, invalid_arg_ctr), return_pairs)))
+                declarations.append((node.name, ((param_types_out, invalid_arg_ctr), return_pairs)))
 
         output_json[py_file] = (declarations, num_of_func)
 
-    with open("test.json", "w") as write_file:
+    with open("scikit.json", "w") as write_file:
         json.dump(output_json, write_file, indent = 4)
