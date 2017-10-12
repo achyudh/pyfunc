238d2372e40ebd486edf695939cf992f50f9595a
azqa <11bscsanadeem@seecs.edu.pk>
2017-10-12 17:31:36 -0400

    Added test cases and initial planting file

---
+++ file_selector/plantMyPy.py
@@ -0,0 +1,32 @@
+
+import sys
+from fileRetriever import Retriever
+from varExtractor import Extractor
+def plantComment(struct):
+	print ("----")
+	for files in struct:
+		for funcs in files:
+			print (funcs)
+			for params in  funcs[1][0]:
+				print (params)
+			print ("***")
+
+'''for returns in funcs[0]:
+print returns'''
+
+
+if __name__ == "__main__":
+    retriever = Retriever()
+    if(len(sys.argv) < 2):
+        raise Exception("Usage: python plantMyPy.py <path\\to\\root\\directory>")
+    root_folder = sys.argv[1]
+    py_files = retriever.list_all_files_in_folder(root_folder)
+
+    output_json = {}
+
+    for py_file in py_files:
+        declarations = Extractor.get_declarations(py_file)
+        output_json[py_file] = declarations
+    print (output_json)
+    #plantComment(output_json)
+	
