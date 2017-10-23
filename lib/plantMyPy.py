6d9be501c578d85abb12b29d2755af826e88f026
Achyudh K Ram <achyudhk@gmail.com>
2017-10-23 16:46:53 +0200

    Auto stash before merge of "master" and "origin/master"

--- lib/plantMyPy.py
+++ lib/plantMyPy.py
@@ -3,95 +3,99 @@ import sys, subprocess, os, re
 from fileRetriever import Retriever
 from varExtractor import Extractor
 def makeComment(struct):
-	comments = {}
-	for path,files in struct.items():
-		#print(path)
-		comments[path] = []
-		for funcs in files:
-			#print ("method name: " + funcs['name'])
-			mypy_str = "# type: ( "
+    comments = {}
+    for path,files in struct.items():
+        #print(path)
+        comments[path] = []
+        for funcs in files:
+            #print ("method name: " + funcs['name'])
+            mypy_str = "# type: ( "
 
-			if len(funcs['params']) > 0:
-				for params in  funcs['params']:
-					#print (params)
-					mypy_str += get_mypy_type(params[1]) + ', ' 
-					#print ("param: " + params)
-				mypy_str = mypy_str[:-2]
-			mypy_str += ') -> '
-			if len(funcs['returns']) > 0:
-				for returns in funcs['returns']:
-					mypy_str += get_mypy_type(returns[1]) + ', ' 
-					#print ("return: " + returns)
-				mypy_str = mypy_str[:-2]
-			else:
-				mypy_str += 'None'
+            if len(funcs['params']) > 0:
+                for params in  funcs['params']:
+                    #print (params)
+                    mypy_str += get_mypy_type(params[1]) + ', '
+                    #print ("param: " + params)
+                mypy_str = mypy_str[:-2]
+            mypy_str += ') -> '
+            if len(funcs['returns']) > 0:
+                for returns in funcs['returns']:
+                    mypy_str += get_mypy_type(returns[1]) + ', '
+                    #print ("return: " + returns)
+                mypy_str = mypy_str[:-2]
+            else:
+                mypy_str += 'None'
 
-			#print (funcs['name'])
-			#print( mypy_str)
-			comments[path].append({'method' : funcs['name'], 'line' : funcs['line'], 'comment': mypy_str})
-			#print ("***")
-	#print(comments)
-	return comments
+            #print (funcs['name'])
+            #print( mypy_str)
+            comments[path].append({'method' : funcs['name'], 'line' : funcs['line'], 'comment': mypy_str})
+            #print ("***")
+    #print(comments)
+    return comments
 
 def get_mypy_type(string):
-	if 'str' in string.lower():
-		return 'str'
-	elif 'int' in string.lower():
-		return 'int'
-	elif 'float' in string.lower() :
-		return 'float'
-	elif 'array' in string.lower() :
-		return 'List'
-	elif 'boolean' in string.lower():
-		return 'bool'
-	elif 'dict' in string.lower():
-		return 'Tuple'
-	else:
-		return 'None'
+    if 'str' in string.lower():
+        return 'str'
+    elif 'int' in string.lower():
+        return 'int'
+    elif 'float' in string.lower() :
+        return 'float'
+    elif 'array' in string.lower() :
+        return 'List'
+    elif 'boolean' in string.lower():
+        return 'bool'
+    elif 'dict' in string.lower():
+        return 'Tuple'
+    else:
+        return 'None'
+
 
 def plantComments(comments):
-	filenames = []
-	for item, value in comments.items():
+    filenames = []
+    for item, value in comments.items():
+        try:
+            f = open(item, 'r')
+            contents = f.readlines()
+            f.close()
+            for comment in value:
+                #contents.insert(item['line']-1, item['comment'])
+                line = contents[comment['line']-1][:-2]
+                line = line + " " + comment['comment'] + "\n"
+                contents[comment['line']-1] = line
 
-		f = open(item, 'r')
-		contents = f.readlines()
-		f.close()
-		for comment in value:
-		#contents.insert(item['line']-1, item['comment'])
-			line = contents[comment['line']-1][:-2]
-			line = line + " " + comment['comment'] + "\n"
-			contents[comment['line']-1] = line
+            contents = ['from typing import Tuple, List \n'] + contents
+            filenames.append(item+'_')
+            f2 = open(item+'_', 'w')
+            contents = "".join(contents)
+            f2.write(contents)
+            f2.close()
+        except:
+            pass
+            #print (contents)
+            #print ("_____")
+    return collect_output(filenames)
 
-		contents = ['from typing import Tuple, List \n'] + contents
-		filenames.append(item+'_')
-		f2 = open(item+'_', 'w')
-		contents = "".join(contents)
-		f2.write(contents)
-		f2.close()
-		#print (contents)
-		#print ("_____")
-	return collect_output(filenames)
 
 def collect_output(filenames):
-	output = []
-	for item in filenames:
-		#output[item] = []
-		#print (item)
-		p= os.popen("mypy --py2 "+item).read()
-		print ('--')
-		#print(p)
-		# for argument mismatch
-		m = re.findall('(.*?):([0-9]+):\s*(error:.*?incompatible type.*)', p, re.IGNORECASE)
-		for (f,l,e) in m:
-			output.append((item, 'parameter', l, e))
+    output = []
+    for item in filenames:
+        #output[item] = []
+        #print (item)
+        p= os.popen("mypy --py2 "+item).read()
+        print ('--')
+        #print(p)
+        # for argument mismatch
+        m = re.findall('(.*?):([0-9]+):\s*(error:.*?incompatible type.*)', p, re.IGNORECASE)
+        for (f,l,e) in m:
+            output.append((item, 'parameter', l, e))
 
-		# return mismatch
-		m = re.findall('(.*?):([0-9]+):\s*(error: missing return statement.*)',  p, re.IGNORECASE)
-		for (f,l,e) in m:		
-			output.append((item, 'return', l,e))
+        # return mismatch
+        m = re.findall('(.*?):([0-9]+):\s*(error: missing return statement.*)',  p, re.IGNORECASE)
+        for (f,l,e) in m:
+            output.append((item, 'return', l,e))
 
-		
-	return output
+
+    return output
 
 
 if __name__ == "__main__":
@@ -115,4 +119,4 @@ if __name__ == "__main__":
         #for item in v:
         print('\n\t type: '+ v + '\n\t\tline: ' + a + '\n\t\t' + b)
 
-	
+
