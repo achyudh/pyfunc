5e16c67a78e470360dbe2855c31d29d8a5e97ae4
mr-spd <spd.bisesser@gmail.com>
2017-10-23 20:28:33 +0200

    Created seperate random sampling file

--- lib/varExtractor.py
+++ lib/varExtractor.py
@@ -21,37 +21,44 @@ class Extractor:
     def get_declarations(py_file):
         global almost_missed
         with open(py_file) as fd:
-            file_contents = fd.read()
-        curr_module = ast.parse(file_contents)
-        func_def_nodes = [node for node in curr_module.body if isinstance(node, ast.FunctionDef)]
-        declarations = list()
+            try:
+                file_contents = fd.read()
+                curr_module = ast.parse(file_contents)
+                func_def_nodes = [node for node in curr_module.body if isinstance(node, ast.FunctionDef)]
+                declarations = list()
 
-        for node in func_def_nodes:
-            if node.name[0] != '_' and ast.get_docstring(node) is not None:
-                docstr_replaced = ast.get_docstring(node).replace("\n", "NEWLINE_PLACEHOLDER")
+                for node in func_def_nodes:
+                    if node.name[0] != '_' and ast.get_docstring(node) is not None:
+                        docstr_replaced = ast.get_docstring(node).replace("\n", "NEWLINE_PLACEHOLDER")
 
-                param_types, return_types = AttrVisitor.get_attr_types(ast.get_docstring(node))
-                param_types_alt, return_types_alt = AttrRegEx.get_attr_types(docstr_replaced)
-                missed_arg_ctr = 0
-                total_arg_ctr = 0
-                param_types_out = list()
-                max_line_num = node.lineno
-                for argument in node.args.args:
-                    if param_types is not None and argument.arg in param_types:
-                        param_types_out.append((argument.arg, param_types[argument.arg]))
-                    elif param_types_alt is not None and argument.arg in param_types_alt:
-                        param_types_out.append((argument.arg, param_types_alt[argument.arg]))
-                        almost_missed += 1
-                    else:
-                        missed_arg_ctr += 1
-                    total_arg_ctr += 1
-                    max_line_num = max(max_line_num, argument.lineno)
+                        param_types, return_types = AttrVisitor.get_attr_types(ast.get_docstring(node))
+                        param_types_alt, return_types_alt = AttrRegEx.get_attr_types(docstr_replaced)
+                        missed_arg_ctr = 0
+                        total_arg_ctr = 0
+                        param_types_out = list()
+                        max_line_num = node.lineno
+                        for argument in node.args.args:
+                            if param_types is not None and argument.arg in param_types:
+                                param_types_out.append((argument.arg, param_types[argument.arg]))
+                            elif param_types_alt is not None and argument.arg in param_types_alt:
+                                param_types_out.append((argument.arg, param_types_alt[argument.arg]))
+                                almost_missed += 1
+                            else:
+                                missed_arg_ctr += 1
+                            total_arg_ctr += 1
+                            max_line_num = max(max_line_num, argument.lineno)
 
-                return_types_alt = list() if return_types_alt is not None else return_types_alt
-                declarations.append({"name": node.name, "line": Extractor.func_defn_endline(py_file, max_line_num), "params": param_types_out,
-                                     "returns": return_types_alt if return_types is None else return_types,
-                                     "count": {"total_params": total_arg_ctr, "missed_params": missed_arg_ctr}})
-        return declarations
+                        return_types_alt = list() if return_types_alt is not None else return_types_alt
+                        declarations.append(
+                            {"name": node.name, "line": Extractor.func_defn_endline(py_file, max_line_num),
+                             "params": param_types_out,
+                             "returns": return_types_alt if return_types is None else return_types,
+                             "count": {"total_params": total_arg_ctr, "missed_params": missed_arg_ctr}})
+                return declarations
+            except:
+                print("READ ERROR:", py_file)
+                return list()
+
 
 
 if __name__ == "__main__":
@@ -59,7 +66,7 @@ if __name__ == "__main__":
     #     raise Exception("Usage: python varExtractor.py <path\\to\\root\\directory>")
     # root_folder = sys.argv[1]
     # py_files = retriever.list_all_files_in_folder(root_folder)
-    py_files = Retriever.list_python_files("../scikit-learn-master/sklearn")
+    py_files = Retriever.list_python_files("../test")
     output_json = {}
 
     total_arg_ctr = 0
