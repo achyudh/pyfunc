3a9a42505553202b25965c5557624ccf6dad547c
Achyudh K Ram <achyudhk@gmail.com>
2017-10-27 14:13:46 +0200

    Add utility function to list all functions with docstring in a module

---
+++ lib/util/parsing.py
@@ -0,0 +1,12 @@
+import ast
+
+
+def fns_with_docstring(file_contents):
+    fn_list = list()
+    curr_module = ast.parse(file_contents)
+    func_def_nodes = [node for node in curr_module.body if isinstance(node, ast.FunctionDef)]
+
+    for node in func_def_nodes:
+        if node.name[0] != '_' and ast.get_docstring(node) is not None:
+            fn_list.append(node.name)
+    return fn_list
